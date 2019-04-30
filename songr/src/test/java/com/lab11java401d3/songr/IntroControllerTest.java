5e9a297fa6ea88bf28e0439fd8f7c64dab008f04
jaezuchowski <jess.zuchowski@gmail.com>
2019-04-29 22:29:12 -0700

    features complete

---
+++ songr/src/test/java/com/lab11java401d3/songr/IntroControllerTest.java
@@ -0,0 +1,22 @@
+package com.lab11java401d3.songr;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class IntroControllerTest {
+
+    @Test
+    public void getCapitalize() {
+        IntroController capitalize = new IntroController();
+
+        String results1 = capitalize.getCapitalize("hey you");
+        assertEquals("HEY YOU", results1);
+
+        String results2 = capitalize.getCapitalize("boop");
+        assertEquals("BOOP", results2);
+
+        String results3 = capitalize.getCapitalize("flowers");
+        assertEquals("FLOWERS", results3);
+    }
+}
